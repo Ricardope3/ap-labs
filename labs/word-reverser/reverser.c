a5ac28357caaad989e5e5181eb66a88c04ba4f75
ricardope3 <ricky.foals@gmail.com>
2020-03-01 21:07:16 -0600

    hola

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char *reverse(char str[], int n);
+int main()
 
-int main(){
-    // Place your magic here
+{
+
+    char str[50], ch;
+
+    int i;
+
+    printf("Enter a string: \n");
+
+    i = 0;
+
+    ch = getchar();
+
+    while (ch != EOF)
+    {
+
+        str[i] = ch;
+
+        i++;
+
+        ch = getchar();
+        if (ch == '\n')
+        {
+            char *reversed;
+            reversed = reverse(str, i);
+            printf("Reversed string is: %s\n", reversed);
+            ch = getchar();
+            i = 0;
+            memset(str, 1,50);
+        }
+    }
     return 0;
+}
+
+char *reverse(char str[], int n)
+{
+    char *new = malloc(50);
+    int j = 0;
+    for (int i = n; i >= 0; i--)
+    {
+        new[j] = str[i];
+        j += 1;
+    }
+    return new;
 }
