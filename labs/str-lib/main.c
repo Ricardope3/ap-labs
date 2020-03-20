18aec15e8895889f1b4e8eedbb81ab07a96f44fa
ricardope3 <ricky.foals@gmail.com>
2020-03-19 18:21:55 -0600

    suicide

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
+#include "strlib.c"
 
-int main() {
+int main(int argc, char **argv)
+{
+    if (argc == 4)
+    {
+        printf("Initial Length      : %d\n", mystrlen(argv[1]));
+        printf("New String          : %s\n", mystradd(argv[1], argv[2]));
+        if (mystrfind(argv[2], argv[3]) == 1)
+        {
+            printf("Substring was found : yes\n");
+        }
+        else
+        {
+            printf("Substring was found : no\n");
+        }
+    }
+    else if (argc == 3)
+    {
+        printf("Initial Length      : %d\n", mystrlen(argv[1]));
+        printf("New String          : %s\n", mystradd(argv[1], argv[2]));
+    }
+    else if (argc == 2)
+    {
+        printf("Initial Length      : %d\n", mystrlen(argv[1]));
+    }
+    else{
+        printf("Usage: ./main.o original addition substring");
+    }
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,56 @@
+#include <stdlib.h>
 
-
-int mystrlen(char *str){
-    return 0;
+int mystrlen(char *str)
+{
+    int i;
+    int counter = 0;
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        counter++;
+    }
+    return counter;
 }
 
-char *mystradd(char *origin, char *addition){
-    return 0;
+char *mystradd(char *origin, char *addition)
+{
+    int n = mystrlen(origin);
+    int m = mystrlen(addition);
+    int k = n + m + 1;
+    char *string = calloc(k, sizeof(string));
+    int i = 0;
+    int j = 0;
+    while (i < n)
+    {
+        string[j] = origin[i];
+        i++;
+        j++;
+    }
+    i = 0;
+    while (i < m)
+    {
+        string[j] = addition[i];
+        i++;
+        j++;
+    }
+    string[k] = '\0';
+    return string;
 }
 
 int mystrfind(char *origin, char *substr){
+    int n = mystrlen(origin);
+    int m = mystrlen(substr);
+    int j = 0;
+    for(int i = 0; i <= n; i++){
+        if(origin[i] == substr[0] && j < 1){
+            j++;
+        } else if(origin[i] == substr[j]){
+            j++;
+        } else {
+            j = 0;
+        }
+        if(j >= m){
+            return 1;
+        }
+    }
     return 0;
 }
