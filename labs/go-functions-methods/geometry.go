444da17ef55b917aeebee5c7c877571471b046f9
ricardope3 <ricky.foals@gmail.com>
2020-03-21 17:23:48 -0600

    funciona

--- labs/go-functions-methods/geometry.go
+++ labs/go-functions-methods/geometry.go
@@ -5,20 +5,29 @@
 
 // Package geometry defines simple types for plane geometry.
 //!+point
-package geometry
+package main
 
-import "math"
+import (
+	"errors"
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
 
-type Point struct{ X, Y float64 }
+//Point struct
+type Point struct{ x, y float64 }
 
-// traditional function
+// Distance traditional function
 func Distance(p, q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Hypot(q.x-p.x, q.y-p.y)
 }
 
-// same thing, but as a method of the Point type
+// Distance same thing, but as a method of the Point type
 func (p Point) Distance(q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Hypot(q.x-p.x, q.y-p.y)
 }
 
 //!-point
@@ -39,4 +48,113 @@ func (path Path) Distance() float64 {
 	return sum
 }
 
+func getRandomPoint() Point {
+	rand.Seed(time.Now().UnixNano())
+	min := -100
+	max := 100
+
+	x := rand.Intn(max-min) + min
+	y := rand.Intn(max-min) + min
+	return Point{float64(x), float64(y)}
+}
+
+func printPoint(p Point) {
+
+	fmt.Printf("X: %f\nY: %f\n", p.x, p.y)
+	fmt.Println("")
+}
+
+func printPath(p Path) {
+	for i := 0; i < len(p); i++ {
+		printPoint(p[i])
+	}
+}
+
 //!-path
+
+func onSegment(p, q, r Point) bool {
+	if q.x < math.Max(p.x, r.x) && q.x > math.Min(p.x, r.x) && q.y < math.Max(p.y, r.y) && q.y > math.Min(p.y, r.y) {
+		return true
+	}
+	return false
+}
+func orientation(p, q, r Point) int {
+	val := (q.y-p.y)*(r.x-q.x) - (q.x-p.x)*(r.y-q.y)
+
+	if val == 0 {
+		return 0
+	}
+	if val > 0 {
+		return 1
+	}
+	return 2
+}
+
+func intersect(p1, q1, p2, q2 Point) bool {
+
+	o1 := orientation(p1, q1, p2)
+	o2 := orientation(p1, q1, q2)
+	o3 := orientation(p2, q2, p1)
+	o4 := orientation(p2, q2, q1)
+	if o1 != o2 && o3 != o4 {
+		return true
+
+	}
+	if o1 == 0 && onSegment(p1, p2, q1) {
+		return true
+	}
+	if o2 == 0 && onSegment(p1, q2, q1) {
+		return true
+	}
+	if o3 == 0 && onSegment(p2, p1, q2) {
+		return true
+	}
+	if o4 == 0 && onSegment(p2, q1, q2) {
+		return true
+	}
+	return false
+}
+
+func candidatePointDoesIntersect(path *Path, candidate Point, i int) bool {
+	p1 := (*path)[i-2]
+	p2 := (*path)[i-1]
+	if intersect(p1, p2, p2, candidate) {
+		return true
+	}
+	return false
+}
+func addPointToPath(path *Path, size int) error {
+	candidate := getRandomPoint()
+	if candidatePointDoesIntersect(path, candidate, size) {
+		return errors.New("")
+	}
+	(*path)[size] = candidate
+	return nil
+}
+func geometry(numSides int) {
+	path := make(Path, numSides)
+	path[0] = getRandomPoint()
+	path[1] = getRandomPoint()
+	size := 2
+	for size < len(path) {
+		err := addPointToPath(&path, size)
+		if err == nil {
+			size++
+		}
+	}
+	printPath(path)
+}
+
+func main() {
+
+	numSides, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Argument bust me an int")
+		os.Exit(1)
+	}
+	if numSides < 3 {
+		fmt.Println("At least three points are needed to create a polygon")
+		os.Exit(1)
+	}
+	geometry(numSides)
+}
