a8cdb3d87d775b1679d5c2e5d4a45d4010a06e3d
ricardope3 <ricky.foals@gmail.com>
2020-03-29 12:05:39 -0600

    logger funcional

---
+++ labs/logger/logger.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int infof(const char *format)
+{
+    printf("\x1b[44m INFO \x1b[0m");
+    printf("\x1b[34m %s \x1b[0m  \n", format);
+    return 1;
+}
+
+int warnf(const char *format)
+{
+    printf("\x1b[30;43m WARNING \x1b[0m");
+    printf("\x1b[33m %s \x1b[0m  \n", format);
+    return 0;
+}
+int errorf(const char *format)
+{
+    printf("\x1b[45m ERROR \x1b[0m");
+    printf("\x1b[35m %s \x1b[0m  \n", format);
+    return 0;
+}
+int panicf(const char *format)
+{
+    printf("\x1b[41m PANIC \x1b[0m");
+    printf("\x1b[31m %s \x1b[0m  \n", format);
+    return 0;
+}
