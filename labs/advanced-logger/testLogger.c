aa3331a6e272cdabf6960826f122fa7ee28839e2
ricardope3 <ricky.foals@gmail.com>
2020-03-31 13:40:04 -0600

    d

--- labs/advanced-logger/testLogger.c
+++ labs/advanced-logger/testLogger.c
@@ -5,20 +5,20 @@ int main(){
 
     // default logging
     infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
+    // warnf("WARN Message %d", 2);
+    // errorf("ERROR Message %d", 2);
 
-    // stdout logging
-    initLogger("stdout");
-    infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
+    // // stdout logging
+    // initLogger("stdout");
+    // infof("INFO Message %d", 1);
+    // warnf("WARN Message %d", 2);
+    // errorf("ERROR Message %d", 2);
 
-    // syslog logging
-    initLogger("syslog");
-    infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
+    // // syslog logging
+    // initLogger("syslog");
+    // infof("INFO Message %d", 1);
+    // warnf("WARN Message %d", 2);
+    // errorf("ERROR Message %d", 2);
 
     return 0;
 }
