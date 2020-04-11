6838607aba2d777db3034c324187cea6c4c2be60
ricardope3 <ricky.foals@gmail.com>
2020-04-10 19:47:14 -0500

    terminar logger

--- labs/logger/testLogger.c
+++ labs/logger/testLogger.c
@@ -1,10 +1,10 @@
 #include "logger.c"
 
 int main() {
-    infof("Ingresando a la funcion main");
-    warnf("Cuidado algo puede estar mal");
-    errorf("Error al tratar de abrir archivo");
-    panicf("Segmentation fault");
+    infof("Ingresando a la funcion main\n");
+    warnf("Cuidado algo puede estar mal\n");
+    errorf("Error al tratar de abrir archivo\n");
+    panicf("Segmentation fault\n");
 
     return 0;
 }
