510baa6d42025312db8b31564919b3da8605d704
ricardope3 <ricky.foals@gmail.com>
2020-04-10 19:37:12 -0500

    s

--- labs/advanced-logger/logger.c
+++ labs/advanced-logger/logger.c
@@ -6,22 +6,6 @@
 #define DEFAULT_LOG 0
 #define SYS_LOG 1
 
-#define RESET 0
-#define BRIGHT 1
-#define DIM 2
-#define UNDERLINE 3
-#define BLINK 4
-#define REVERSE 7
-#define HIDDEN 8
-
-#define BLACK 0
-#define RED 1
-#define GREEN 2
-#define YELLOW 3
-#define BLUE 4
-#define MAGENTA 5
-#define CYAN 6
-#define WHITE 7
 
 int logFlag = DEFAULT_LOG;
 
