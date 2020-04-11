5ebcb07f995c519baf3116ab5dfc1657823cfde7
ricardope3 <ricky.foals@gmail.com>
2020-04-10 19:29:00 -0500

    terminar lab de advanced logger

--- labs/advanced-logger/logger.c
+++ labs/advanced-logger/logger.c
@@ -6,6 +6,23 @@
 #define DEFAULT_LOG 0
 #define SYS_LOG 1
 
+#define RESET 0
+#define BRIGHT 1
+#define DIM 2
+#define UNDERLINE 3
+#define BLINK 4
+#define REVERSE 7
+#define HIDDEN 8
+
+#define BLACK 0
+#define RED 1
+#define GREEN 2
+#define YELLOW 3
+#define BLUE 4
+#define MAGENTA 5
+#define CYAN 6
+#define WHITE 7
+
 int logFlag = DEFAULT_LOG;
 
 int initLogger(char *logType, ...)
@@ -15,7 +32,6 @@ int initLogger(char *logType, ...)
     {
         logFlag = SYS_LOG;
         openlog("Advanced Logger -", LOG_CONS, LOG_USER);
-        syslog(LOG_INFO, "MIRA AQUI ES UNA PREUBA POR FAVOR VEME LMAO");
         printf("Initializing Logger on: %s\n", logType);
     }
     else
@@ -28,36 +44,70 @@ int initLogger(char *logType, ...)
 
 int infof(const char *format, ...)
 {
-        va_list list;
-        va_start(list, format);
+    va_list list;
+    va_start(list, format);
     if (logFlag == DEFAULT_LOG)
     {
-        printf("\x1b[44m INFO \x1b[0m    ");
-        // printf("\x1b[34m %s \x1b[0m  \n", format);
+
+        printf("\x1b[44m INFO \x1b[0m     ");
         vprintf(format, list);
-    }else{
-        syslog(LOG_INFO, "\x1b[44m INFO \x1b[0m    ");
-        syslog(LOG_INFO, format,list);
+    }
+    else
+    {
+        syslog(LOG_INFO, format, list);
     }
     return 0;
 }
 
 int warnf(const char *format, ...)
 {
-    printf("\x1b[30;43m WARNING \x1b[0m");
-    printf("\x1b[33m %s \x1b[0m  \n", format);
+
+    va_list list;
+    va_start(list, format);
+    if (logFlag == DEFAULT_LOG)
+    {
+
+        printf("\x1b[30;43m WARNING \x1b[0m  ");
+        vprintf(format, list);
+    }
+    else
+    {
+        syslog(LOG_WARNING, format, list);
+    }
+
     return 0;
 }
 int errorf(const char *format, ...)
 {
-    printf("\x1b[45m ERROR \x1b[0m");
-    printf("\x1b[35m %s \x1b[0m  \n", format);
+
+    va_list list;
+    va_start(list, format);
+    if (logFlag == DEFAULT_LOG)
+    {
+        printf("\x1b[45m ERROR \x1b[0m    ");
+        vprintf(format, list);
+    }
+    else
+    {
+        syslog(LOG_ERR, format, list);
+    }
+
     return 0;
 }
 int panicf(const char *format, ...)
 {
-    printf("\x1b[41m PANIC \x1b[0m");
-    printf("\x1b[31m %s \x1b[0m  \n", format);
+
+    va_list list;
+    va_start(list, format);
+    if (logFlag == DEFAULT_LOG)
+    {
+        printf("\x1b[41m PANIC \x1b[0m    ");
+        vprintf(format, list);
+    }
+    else
+    {
+        syslog(LOG_EMERG, format, list);
+    }
     return 0;
 }
 
--- labs/advanced-logger/testLogger.c
+++ labs/advanced-logger/testLogger.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include "logger.h"
 
-int main(){
+int main()
+{
 
     // default logging
     // infof("INFO Message %d\n", 1);
-    // warnf("WARN Message %d", 2);
-    // errorf("ERROR Message %d", 2);
-
+    // warnf("WARN Message %d\n", 2);
+    // errorf("ERROR Message %d\n", 2);
+    // panicf("PANIC Message %d\n",1);
     // // stdout logging
+    initLogger("stdout");
+    infof("INFO Message  %d\n", 1);
+    warnf("WARN Message %d\n", 2);
+    errorf("ERROR Message %d\n", 2);
+    panicf("PANIC Message %d\n", 1);
+
+    // syslog logging
     initLogger("syslog");
     infof("INFO Message %d", 1);
-    // warnf("WARN Message %d", 2);
-    // errorf("ERROR Message %d", 2);
-
-    // // syslog logging
-    // initLogger("syslog");
-    // infof("INFO Message %d", 1);
-    // warnf("WARN Message %d", 2);
-    // errorf("ERROR Message %d", 2);
-
+    warnf("WARN Message %d", 2);
+    errorf("ERROR Message %d", 2);
+    panicf("PANIC Message %d\n", 1);
     return 0;
 }
