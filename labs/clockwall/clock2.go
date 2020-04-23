cf7e6d11b7fdf22fba764a12f7dfc97c353386ee
ricardope3 <ricky.foals@gmail.com>
2020-04-23 16:02:07 -0500

    primer commit

--- labs/clockwall/clock2.go
+++ labs/clockwall/clock2.go
@@ -2,34 +2,45 @@
 package main
 
 import (
-	"io"
-	"log"
-	"net"
-	"time"
+	// "io"
+	// "log"
+	// "net"
+	// "time"
+	"os"
+	"fmt"
 )
 
-func handleConn(c net.Conn) {
-	defer c.Close()
-	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
-		if err != nil {
-			return // e.g., client disconnected
-		}
-		time.Sleep(1 * time.Second)
-	}
-}
+// func handleConn(c net.Conn) {
+// 	defer c.Close()
+// 	for {
+// 		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+// 		if err != nil {
+// 			return // e.g., client disconnected
+// 		}
+// 		time.Sleep(1 * time.Second)
+// 	}
+// }
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:9090")
-	if err != nil {
-		log.Fatal(err)
+	tz := os.Getenv("TZ")
+	if len(os.Args) < 2 || len(tz) <= 0 {
+		fmt.Println("Usage: TZ=<timezone> go run clock2.go -port <port>")
+		os.Exit(1)
 	}
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Print(err) // e.g., connection aborted
-			continue
-		}
-		go handleConn(conn) // handle connections concurrently
-	}
+	port := os.Args[2]
+	fmt.Println(tz)
+	fmt.Println(port)
+	
+	// listener, err := net.Listen("tcp", "localhost:9090")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// for {
+	// 	conn, err := listener.Accept()
+	// 	if err != nil {
+	// 		log.Print(err) // e.g., connection aborted
+	// 		continue
+	// 	}
+	// 	go handleConn(conn) // handle connections concurrently
+	// }
 }
