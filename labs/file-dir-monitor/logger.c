ef0e7666fc95ec63051c06b62f9d7c213f4d5ea3
ricardope3 <ricky.foals@gmail.com>
2020-05-09 12:18:42 -0500

    first commit:

--- labs/file-dir-monitor/logger.c
+++ labs/file-dir-monitor/logger.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include "logger.h"
 
 int initLogger(char *logType) {
@@ -6,18 +7,37 @@ int initLogger(char *logType) {
     return 0;
 }
 
-int infof(const char *format, ...) {
+int infof(const char *format, ...)
+{
+    va_list list;
+    va_start(list, format);
+    printf("\x1b[44m INFO \x1b[0m");
+    vprintf(format, list);
     return 0;
 }
 
-int warnf(const char *format, ...) {
+int warnf(const char *format, ...)
+{
+    va_list list;
+    va_start(list, format);
+    printf("\x1b[30;43m WARNING \x1b[0m");
+    vprintf(format, list);
     return 0;
 }
-
-int errorf(const char *format, ...) {
+int errorf(const char *format, ...)
+{
+    va_list list;
+    va_start(list, format);
+    printf("\x1b[45m ERROR \x1b[0m");
+    vprintf(format, list);
     return 0;
 }
-int panicf(const char *format, ...) {
+int panicf(const char *format, ...)
+{
+    va_list list;
+    va_start(list, format);
+    printf("\x1b[41m PANIC \x1b[0m");
+    vprintf(format, list);
     return 0;
 }
 
